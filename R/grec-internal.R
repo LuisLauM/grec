a437d283fed0469792fef2b326c82a1009aaf0be
Wencheng Lau-Medrano <luis.laum@gmail.com>
2017-07-01 23:31:26 -0500

    First commit. Functional version.

---
+++ R/grec-internal.R
@@ -0,0 +1,132 @@
+checkArgs <- function(grecArgs, type){
+
+  output <- switch(type,
+                   frontDetect = checkArgs_frontDetect(grecArgs),
+                   "Invalid value for 'type'.")
+
+  return(output)
+}
+
+
+checkArgs_frontDetect <- function(allArgs){
+
+  envirData <- allArgs$envirData
+  thresholds <- allArgs$thresholds
+  stepByStep <- allArgs$stepByStep
+
+  msg1 <- "If 'envirData' is a list, it must contain info for environmental map. See help(frontDetect)."
+  msg2 <- "If 'envirData' is a matrix, it must be a numerical matrix with environmental data. See help(frontDetect)."
+  msg3 <- "'envirData' must a matrix or list containing numerical values of environmental map. See help(frontDetect)."
+
+  if(is.list(envirData)){
+
+    # Check if envirData is a list with 'x', 'y', 'z' dimensions, where z is a numeric matrix
+    index <- (length(envirData) == 3 && all(is.element(c("x", "y", "z"), letters)) &&
+                is.matrix(envirData$z) && is.numeric(envirData$z))
+    if(!index){
+      stop(msg1)
+    }
+  }else if(is.matrix(envirData)){
+
+    # Check if envirData is a valid numerical matrix
+    index <- is.matrix(envirData$z) && is.numeric(envirData$z)
+    if(!index){
+      stop(msg2)
+    }
+  }else{
+    stop(msg3)
+  }
+
+  msg4 <- "Invalid value for 'stepByStep', it will take its default value (TRUE)."
+  if(!is.logical(stepByStep)){
+    stepByStep <- TRUE
+    warning(msg4)
+  }
+
+  # Check if thresholds is a numeric vector of length 1 or 2
+  msg5 <- "'thresholds' must be a numeric vector."
+  if(!is.numeric(thresholds)){
+    stop(msg5)
+  }
+
+  msg6 <- "'thresholds' must be a numeric vector of length 1 or 2. See help(frontDetect)."
+  if(length(thresholds) == 1){
+    thresholds <- c(thresholds, 5*thresholds)
+  }else if(length(thresholds) == 2){
+    thresholds <- sort(thresholds)
+  }else{
+    stop(msg6)
+  }
+
+  return(allArgs)
+}
+
+frontDetect_internal <- function(envirData, thresholds, stepByStep, extraParams){
+
+  # Create empty list for outputs
+  if(stepByStep){
+    output <- list()
+    output[[1]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = envirData$z)
+  }
+
+  # Make a first smooth
+  preMatrix <- medianFilter(dataMatrix = envirData$z,
+                            radius = extraParams$firstSmooth$radius,
+                            times = extraParams$firstSmooth$times)
+
+  if(stepByStep){
+    output[[2]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = preMatrix)
+  }
+
+  # Define sobel kernel values
+  sobelKernel <- extraParams$sobelStrength*c(-1, -2, -1, 0, 0, 0, 1, 2, 1)
+
+  # Define sobel kernels
+  sobelH <- matrix(data = sobelKernel, nrow = 3, byrow = TRUE)
+  sobelV <- matrix(data = sobelKernel, nrow = 3, byrow = FALSE)
+
+  # Apply sobel filters (horizontal and vertical)
+  filteredH <- convolution2D(dataMatrix = preMatrix, kernel = sobelH, noNA = TRUE)
+  filteredV <- convolution2D(dataMatrix = preMatrix, kernel = sobelV, noNA = TRUE)
+
+  if(stepByStep){
+    output[[3]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = filteredH)
+    output[[4]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = filteredV)
+  }
+
+  # Calculate gradient
+  newSobel <- sqrt(filteredH^2 + filteredV^2)
+  newSobel[newSobel < thresholds[1] | newSobel > thresholds[2]] <- NA
+
+  if(stepByStep){
+    output[[5]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = newSobel)
+  }
+
+  # Clear noisy signals
+  newSobel <- medianFilter(dataMatrix = newSobel,
+                           radius = extraParams$clearNoise$radius,
+                           times = extraParams$clearNoise$times)
+
+  if(stepByStep){
+    output[[6]] <- list(x = envirData$x,
+                        y = envirData$y,
+                        z = newSobel)
+    names(output) <- c("original", "first_smooth", "sobel_H", "sobel_V", "gradient", "noise_cleared")
+  }else{
+    output <- list(x = envirData$x,
+                   y = envirData$y,
+                   z = newSobel)
+  }
+
+  return(output)
+}
---
+++ R/grec-main.R
@@ -0,0 +1,74 @@
+#' @title Classification of spatial patterns from environmental data through GRadient RECognition
+#' @import imagine
+#'
+#' @author Wencheng Lau-Medrano, \email{luis.laum@gmail.com}
+#' @name grec-package
+#' @description It provides algorithms for detection of spatial patterns from oceanographic data.
+#' @aliases grec-package grec
+#' @docType package
+#' @keywords gradient, detection, environmental-data,
+NULL
+
+#' @title Default color palette most using on environmental representations.
+#' @name colPalette
+#' @description Vector with 2000 colors generated from \code{fields::tim.colors} function.
+#' @aliases colPalette
+#' @docType data
+#' @usage colPalette
+#' @format A vector of 2000 colors in RBG format.
+#' @references \code{fields} package
+NULL
+
+#' @title Detection of fronts based on gradient recognition
+#'
+#' @param envirData Either a list o numerical matrix with environmental info. See Details.
+#' @param thresholds \code{numeric} vector of length 1 or 2 with info of limits of values to consider. See Details.
+#' @param stepByStep \code{logical} indicating whether to get the intermediate matrices (\code{TRUE})
+#' or just the final one (\code{FALSE}).
+#'
+#' @details That function performs 4 steps:
+#' \enumerate{
+#' \item Smoothing of the original data.
+#' \item Application of sobel filters horizontally (sobelH) and vertically (sobelV).
+#' \item Extract gradients, using the formula \eqn{sqrt(sobelH^2 + sobelV^2)}.
+#' \item Removing noise signals using a median filter, from \link{\code{imagine}} package.
+#' }
+#'
+#' In order to improve the extraction of fronts, the package allows users to change the \code{thresholds}
+#' values. It is better detailed at vignettes.
+#'
+#' \code{envirData} could be given as a single numeric matrix containing the values of a
+#' environmental map. Othersiwe it also can be a list with dimensions 'x', 'y' and 'z' specifying
+#' the dimensions of the data as folloews: 'x' will be a numeric vector with the values of longitude,
+#' 'y' will indicate the latitude (numeric vector as well). 'grec' package does not be rigorous in
+#' the check of the values given for dimensions, so the user must be carefull with them.
+#'
+#' \code{thresholds} could be given as a single value. If so, the second value must be calculated
+#' as a 5 times the given value. That argument must be applied after the smoothing (step 1).
+#'
+#' @return Depending on \code{stepByStep} argument, it can be a list or a single numeric matrix.
+#' @export
+#'
+#' @examples
+#' load(system.file("extdata", "exampleSSTData.RData", package = "grec"))
+#' out <- frontDetect(envirData = exampleSSTData, threshold = c(10, 500), stepByStep = FALSE)
+#' image(out, col = colPalette)
+frontDetect <- function(envirData, thresholds, stepByStep = TRUE){
+  # Check and validation of arguments
+  checkedArgs <- list(envirData = envirData, thresholds = thresholds, stepByStep = stepByStep)
+  checkedArgs <- checkArgs(grecArgs = checkedArgs, type = as.character(match.call())[1])
+
+  # Define extra parameters for filter
+  extraParams <- list(firstSmooth = list(radius = 5,
+                                         times = 10),
+                      sobelStrength = 10,
+                      clearNoise = list(radius = 5,
+                                        times = 1))
+
+  # Apply filters
+  output <- with(checkedArgs,
+                 frontDetect_internal(envirData = envirData, thresholds = thresholds, stepByStep = stepByStep,
+                                      extraParams = extraParams))
+
+  return(output)
+}
